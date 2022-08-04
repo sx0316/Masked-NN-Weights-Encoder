beb4b5a04e00b2ee38cd3cfecd0402731c026d66
sx0316 <z18516937829@163.com>
2022-08-04 23:51:54 +0800

    Changed Target Dataset to CIFAR10

--- weight_triplets_img_input.py
+++ weight_triplets_img_input.py
@@ -14,19 +14,24 @@ import torch.optim as optim
 import torch.utils.data as data
 import torchvision
 import torchvision.transforms as transforms
-from torchvision.datasets import MNIST
+from torchvision.datasets import MNIST, ImageNet, CIFAR10
 import torchvision.models as models
 import torch.utils.data.dataloader as dataloader
 import numpy as np
 from torch.utils.data import DataLoader
 from torchmetrics import MeanSquaredError
+from cifar10_models.resnet import resnet18, resnet34, resnet50
+from torch.utils.data.dataloader import default_collate
+
 
 import torch
 
 batch_size = 2
-max_size = 128
+max_size = 512
 torch.set_default_dtype(torch.double)
 img_batch_size = 10
+#d0 = torch.device("cuda:0")
+#d1 = torch.device("cuda:1")
 
 def get_np_fixed_length(list_like, length):
     list_length = len(list_like)
@@ -39,8 +44,10 @@ def get_np_fixed_length(list_like, length):
 # 1. Generate a series of masked matrices
 def load_pretrained_weight_matrices(num_matrices):
     # Option 1: passing weights param as string
-    pretrained_model = torch.hub.load("pytorch/vision", "resnet50", weights="IMAGENET1K_V2")
+    #pretrained_model = torch.hub.load("pytorch/vision", "resnet50", weights="IMAGENET1K_V2")
     # cache is in /root/.cache/torch/hub/pytorch_vision_main
+    #pretrained_model = resnet50()
+    pretrained_model = resnet50(pretrained=True)
     transform = transforms.Compose([transforms.ToTensor()])
     # initialzie the training and validation dataset
     print("[INFO] loading the training and validation dataset...")
@@ -62,7 +69,7 @@ def load_pretrained_weight_matrices(num_matrices):
                 continue
             if (cur.shape[0] > max_size) or (cur.shape[1] > max_size) or (len(cur.shape) > 1 and cur.shape[3] > 3) : continue
             print("start")
-            print(name)
+            print(name, cur.shape)
 
             mask = np.random.rand(*cur.shape)
             bool_mask = mask < 0.75
@@ -97,6 +104,7 @@ class RNNDataloader(dataloader.DataLoader):
     def __init__(self, data, batch_size, shuffle=True, num_workers=1):
         super(dataloader.DataLoader, self).__init__()
         self.data = data
+        self.data = self.data
         self.batch_size = 1  # batch_size
         self.shuffle = shuffle
         self.num_workers = num_workers
@@ -136,7 +144,10 @@ class RNNDataloader(dataloader.DataLoader):
         Format the data.
         """
         data = torch.squeeze(torch.cat(data.unbind()).unsqueeze(0))
-        return torch.Tensor(data).to(torch.double)
+        data = torch.Tensor(data)
+        data = data.double()#.to(d0)
+       # print(data.get_device())
+        return data
 
 class RNN(nn.Module):
     """
@@ -155,32 +166,36 @@ class RNN(nn.Module):
         self.output_layer = nn.Linear(hidden_size*3, output_size)
         self.attn = nn.MultiheadAttention(hidden_size, hidden_size)
         self.img_batch_size = 8
+        self.dummy_param = nn.Parameter(torch.empty(0))
+
         self.resize_sz = 224
-        self.src_dataset = MNIST(root="./mnist", train=True,
-                                 download=True, transform= transforms.Compose([transforms.Resize(self.resize_sz),
+        self.src_dataset = CIFAR10(root="~/matrix_filling/", train=True,
+                                 download=False, transform= transforms.Compose([transforms.Resize(self.resize_sz),
                                                transforms.ToTensor(),
                                                ]))
         self.trainDataLoader = DataLoader(self.src_dataset, batch_size=self.img_batch_size, shuffle=True)
         self.img_batch = next(iter(self.trainDataLoader))
         self.src_img = self.img_batch[0]#.squeeze(1)
-        self.src_img = self.src_img.squeeze(1)
-        self.src_img = torch.flatten(self.src_img, start_dim=0, end_dim=1)
+        print(self.src_img.shape)
+        #self.src_img = self.src_img.squeeze(1)
+        self.src_img = torch.flatten(self.src_img, start_dim=0, end_dim=2)
         self.src_img = torch.permute(self.src_img, (1,0))
+        self.src_img = self.src_img.to(torch.device("cpu"))#.to(d0)
         self.img_layer = nn.Linear(self.src_img.shape[1],self.hidden_size)
         self.coordinating_layer = nn.Bilinear(self.resize_sz, self.hidden_size*3,self.hidden_size*3)
 
 
-    def forward(self, input, hidden):
+    def forward(self, input):
         """
         Forward pass of the RNN.
         """
      #   print(len(input))
-        input = input[0]
-        input__ = input[0].to(torch.double).clone()
+      #  input = input[0]
+        input__ = input[0].double().clone()#.to(d0)
         input_ = self.input_layer(input__)
-        prev = input[1].to(torch.double).clone()
+        prev = input[1].double().clone()#.to(d0)
         prev_ = self.prev_layer(prev)
-        after = input[2].to(torch.double).clone()
+        after = input[2].double().clone()
         after_ = self.after_layer(after)
         img_ = self.img_layer(self.src_img)
         img_ = torch.permute(img_, (1, 0))
@@ -189,10 +204,11 @@ class RNN(nn.Module):
 
         output = self.attn(prev_, after_, input_)[0]
         output = torch.permute(output, (1,0))
+        print("attn_out:", output.shape)
         coord = self.coordinating_layer(img_, output)
 
         output = torch.permute(coord, (1,0))
-        return output, hidden
+        return output
 
     def init_hidden(self, batch_size):
         """
@@ -217,7 +233,7 @@ def train(model, dataloader, validation_dl, num_epochs, learning_rate):
     # 1. Define the loss and optimizer
     #criterion = nn.CrossEntropyLoss()
     criterion = nn.MSELoss()
-    hidden = model.init_hidden(9)
+    #hidden = model.init_hidden(9)
 
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     optimizer.zero_grad()
@@ -230,7 +246,12 @@ def train(model, dataloader, validation_dl, num_epochs, learning_rate):
 
             optimizer.zero_grad()
             # 5. Forward pass
-            outputs, hidden = model(inputs, hidden.clone())
+            print(model.dummy_param.device)
+            inputs_ = inputs[0]
+            for i in inputs_:
+              i.to(torch.device("cpu"))
+              print(i.get_device())
+            outputs = model(inputs_)
             # 6. Compute the loss
             print("loss", targets[0].shape)
             loss = criterion(outputs.clone(), targets[0].clone())
@@ -274,10 +295,10 @@ def load_model():
 
 
 data = load_pretrained_weight_matrices(100)
+print("Data Size:", len(data))
 test_size = int(len(data) * 0.91)
 train_set, val_set = data[:test_size], data[test_size:-1]
 model = RNN(max_size, max_size, max_size)
 dataloader_ = RNNDataloader(train_set, batch_size=batch_size)
 validation_dataloader_ = RNNDataloader(val_set, batch_size=batch_size)
-model = train(model, dataloader_, validation_dataloader_, num_epochs=100, learning_rate=0.001)
-
+tmodel = train(model, dataloader_, validation_dataloader_, num_epochs=100, learning_rate=0.001)
